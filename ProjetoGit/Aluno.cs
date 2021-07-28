0b788263b0278b91a71ed9231be74e98625fe908
Francisca Kátia Barbosa de Souza <fkbs04@hotmail.com>
2021-07-28 16:01:49 -0300

    classe aluno

---
+++ ProjetoGit/Aluno.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoGit
+{
+    class Aluno
+    {
+        public int Id { get; set; }
+        public  string Nome { get; set; }
+    }
+}
