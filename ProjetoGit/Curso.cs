1e7ee78c3459ebb12b34b22551ed0e8a6fc79ad5
Francisca Kátia Barbosa de Souza <fkbs04@hotmail.com>
2021-07-28 16:06:16 -0300

    Nova classe curso

---
+++ ProjetoGit/Curso.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoGit
+{
+    class Curso
+    {
+        public int Id { get; set; }
+        public string NomeCurso1 { get; set; }
+    }
+}
